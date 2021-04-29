b67f0dc664bc6f71046db8b24bc63a27d6dc771c
Abhijith Ashokan <81083505+Abhijith-Ashokan@users.noreply.github.com>
2021-04-29 20:53:57 +0530

    Create activity3.c

---
+++ src/activity3.c
@@ -0,0 +1,25 @@
+/**
+ * @file activity3.c
+ * @author Abhijith Ashokan (abhijithashokan99@gmail.com)
+ * @brief library containing PWM port definitions
+ * @version 0.1
+ * @date 2021-04-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+#include "activity3.h"
+
+/**
+ * @brief PWM port definitions
+ * 
+ */
+void portPWM(){
+
+    TCCR1A |= NonInveringPWM|ModeSelection2|ModeSelection1;
+    TCCR1B |= ModeSelection3|PWMPrescalar;
+    DDRB |= LEDPWM;
+
+}
